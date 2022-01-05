51145ef00ee4cd8c2bc888efd4a047552bcf4952
Gamaliel Burgos <gamyburgos@gmail.com>
2022-01-04 19:14:51 -0800

    Properties and Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // this is the tag that is embedded
   selector: 'app-root',
+  // the html file that we are using
   templateUrl: './app.component.html',
+  // this is the style that we are using
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gamy-directory';
+  // adding a type 
+  title: string = 'Task Tracker';
 }
